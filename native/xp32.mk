15180d53361ea898076092690e04e4effe1afab6
T. Corbin <corbint@uk.ibm.com>
2017-07-04 09:21:19 +0100

    remove native code from master branch

--- native/xp32.mk
+++
@@ -1,5 +0,0 @@
-#include makefile for Linux
-include xi32.mk
-#CC=gcc
-#LINK=gcc
-CFLAGS+=-m32 -D_PPC -DREVERSED
--- native/xz64.mk
+++
@@ -1,3 +0,0 @@
-include xa64.mk
-LINK_OPT+=-m64
-CFLAGS+=-m64 -D_S390 -D_64BIT -DREVERSED
